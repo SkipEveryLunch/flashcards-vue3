da95140dff5887e6cd80629d6317d15e43269ac1
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-08-22 23:01:17 +0900

    implemented Header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
+  <Header />
   <h1 class="bg-gray-200">Hi</h1>
 </template>
+<script>
+import Header from './components/Header/Header.vue';
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+};
+</script>

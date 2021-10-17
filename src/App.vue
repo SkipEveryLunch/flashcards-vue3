078492ec3f1988e4d0d773870800a1c53a4bccd9
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-10-17 17:00:42 +0900

    fixed main.ts

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-black">
-    <FavButton type="like" :count="3" :isFaved="isFaved" @fav="onFav" />
-  </div>
+  <router-view />
 </template>
 <script>
-import FavButton from './components/FavButton.vue';
-import { ref } from 'vue';
 export default {
   name: 'App',
-  components: {
-    FavButton,
-  },
-  setup() {
-    const isFaved = ref(false);
-    const onFav = () => {
-      isFaved.value = !isFaved.value;
-    };
-    return {
-      isFaved,
-      onFav,
-    };
-  },
 };
 </script>

9c06737f4d516768f6b6c0bab812c70b4211b4a4
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-12-29 15:42:10 +0900

    change the design of commentcard

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -7,22 +7,29 @@
     </div>
     <div class="triangle"></div>
     <div class="card" data-testId="comment-card">
-      <div class="flex flex-col w-full px-5 py-3">
-        <div class="mb-1">
-          <p>
-            {{ comment.comment_type.name }}
-          </p>
-        </div>
-        <div class="text-sm">
-          <p>{{ comment.comment_detail }}</p>
+      <div class="flex w-full px-5 py-3">
+        <div class="flex flex-col pr-2">
+          <div class="mb-1">
+            <p>
+              {{ comment.comment_type.name }}
+            </p>
+          </div>
+          <div class="text-sm">
+            <p>{{ comment.comment_detail }}</p>
+          </div>
         </div>
       </div>
-      <button
+      <div
         v-if="user.role.id === 1"
-        @click="() => deleteComment(comment.id)"
+        class="flex items-end justify-end px-3 py-2"
       >
-        削除する
-      </button>
+        <button
+          class="btn btn-primary"
+          @click="() => deleteComment(comment.id)"
+        >
+          削除する
+        </button>
+      </div>
     </div>
   </div>
 </template>
